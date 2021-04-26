9ff16e92a6395f625f00a1d872029f3b5befb1fa
gdsilva1 <68176472+gdsilva1@users.noreply.github.com>
2021-04-26 13:39:54 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -7,16 +7,16 @@ sudo dnf update -y
 sudo dnf upgrade -y
 
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf groupupdate core -y
+sudo dnf groupupdate core
 
-sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
-sudo dnf groupupdate sound-and-video -y
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf groupupdate sound-and-video
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak install flathub com.spotify.Client -y
 flatpak install flathub com.discordapp.Discord -y
 
-sudo dnf install steam freecad inkscape gnome-tweaks texlive-scheme-full mozilla-fira-sans-fonts texlive-tex-gyre-doc papirus-icon-theme zsh piper
+sudo dnf install steam freecad inkscape gnome-tweaks texlive-scheme-full mozilla-fira-sans-fonts texlive-tex-gyre-doc papirus-icon-theme zsh piper -y
 
 
