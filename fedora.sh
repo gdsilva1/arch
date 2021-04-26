a2cb264a67988862ef70c8c7d1a6528bf31967a6
gdsilva1 <68176472+gdsilva1@users.noreply.github.com>
2021-04-26 16:45:45 -0300

    Comentários no arquivo

--- fedora.sh
+++ fedora.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
+# Adicionando respositórios mais rápidos
+
 echo "fastestmirror=true" >> /etc/dnf/dnf.conf
 echo "deltarpm=true" >> /etc/dnf/dnf.conf
+
+# Atualizando o sistema (útil principalmente se a instalação não for feita pela ISO do Fedora Server)
 
 sudo dnf update -y
 sudo dnf upgrade -y
 
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf groupupdate core
+# Pacotes do RPM Fusion
 
-sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-sudo dnf groupupdate sound-and-video
+sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf -y groupupdate core
+
+sudo dnf -y groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf -y groupupdate sound-and-video
+
+# Adicionando repositórios fo Flathub
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# Instalando pacotes em Flatpak
+
 flatpak install flathub com.spotify.Client -y
 flatpak install flathub com.discordapp.Discord -y
+
+# Instalando pacotes do repositório
 
 sudo dnf install steam freecad inkscape gnome-tweaks texlive-scheme-full mozilla-fira-sans-fonts texlive-tex-gyre-doc papirus-icon-theme zsh piper -y
 
