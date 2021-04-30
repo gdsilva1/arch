c56cad255ff2e23fa717f1ae24a107dd04b0b128
gdsilva1 <68176472+gdsilva1@users.noreply.github.com>
2021-04-29 21:28:38 -0300

    Plugins zsh

--- Fedora/fedora_user.sh
+++ Fedora/fedora_user.sh
@@ -13,3 +13,6 @@ flatpak install flathub com.discordapp.Discord -y
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
