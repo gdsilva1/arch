e49ef8e6e9d65e47554f0bee1137083483a077c1
gdsilva1 <68176472+gdsilva1@users.noreply.github.com>
2022-03-06 08:46:25 -0300

    Update arch-base.sh

--- Arch/arch-base.sh
+++ Arch/arch-base.sh
@@ -9,10 +9,10 @@ sed -i '394s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=br-abnt2" >> /etc/vconsole.conf
-echo "legion" >> /etc/hostname
+echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost.localdomain localhost" >> /etc/hosts
 echo "::1 localhost.localdomain localhost" >> /etc/hosts
-echo "127.0.1.1 legion.localdomain legion" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 
 # Configuração do pacman
 mv /etc/pacman.conf /etc/pacman.conf.backup
